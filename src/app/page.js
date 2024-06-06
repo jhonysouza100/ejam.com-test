8353705499879ad2b95d45e1f27f724dd6a563a7
jhonysouza100 <jhonatansouzameza100@gmail.com>
2024-06-06 18:36:52 -0300

    060620241834

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
-import { RiCheckboxCircleFill, RiNumber3, RiNumber4 } from "@remixicon/react";
+import { RiCheckboxCircleFill, RiNumber3, RiNumber4, RiStarFill } from "@remixicon/react";
 import Image from "next/image";
 
 function page() {
@@ -10,7 +10,7 @@ function page() {
       <main className="container px-4 bg-body text-main">
         <section className="text-wrap py-8 space-y-2">
           <h1 className="text-2xl font-semibold text-center">Wait! Your Order In Progress.</h1>
-          <p className="text-sm text-center text-zinc-600">Lorem Impsum Dolor Amet, Consectetur Adipscing</p>
+          <p className="text-sm text-center text-zinc-500">Lorem Impsum Dolor Amet, Consectetur Adipscing</p>
         </section>
 
         <section className="container px-4 grid grid-cols-4 gap-x-12">
@@ -28,12 +28,12 @@ function page() {
         </section>
 
         <section className="py-8">
-          <div className="grid grid-cols-3 gap-x-4">
+          <div className="grid grid-cols-3 gap-x-3">
             <Image  className="col-span-1 py-2 bg-blue-600 rounded-xl" src='/images/main-image.webp' width={100} height={100} alt="clarifion image" />
-            <div className="col-span-2 flex-col items-end justify-between">
-              <span className="text-nowrap">Clarifion Air lonizer <span className="text-zinc-600 text-xs">$180 <span className="text-blue-600 text-sm">$84</span></span></span>
-              <div></div>
-              <span></span>
+            <div className="col-span-2 py-2 flex flex-col items-start justify-between">
+              <span className="text-nowrap space-x-3"><span className="-tracking-wide">Clarifion Air lonizer</span><span className="text-zinc-600 text-xs"><del>$180</del> <span className="text-blue-600 text-sm font-semibold">$84</span></span></span>
+              <div className="text-yellow-400 flex"><RiStarFill className="w-3 h-3"/><RiStarFill className="w-3 h-3"/><RiStarFill className="w-3 h-3"/><RiStarFill className="w-3 h-3"/><RiStarFill className="w-3 h-3"/></div>
+              <span className="text-zinc-500"><div className="after:contet-[''] after:bg-blue-600 after:w-2 after:h-2 after:rounded-full after:shadow-md after:shadow-blue-300"></div> <span className="-tracking-wide">12 left in Stock</span></span>
             </div>
           </div>
           <div>
